dd50ecf7a73708eb179ee0c8d7dd8f22023c51e0
nicomarklaguitan0 <nicomarklaguitan0@gmail.com>
2026-06-30 21:57:55 +0800

    added apis

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Index } from './Index.jsx'
-import './main.css'
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { Index } from "./Index.jsx"
+import "./main.css"
+import { Amplify } from "aws-amplify"
 
-Amplify.configure(outputs);
+// Try to load Amplify config safely (won't crash if missing in production)
+let outputs = null
 
-createRoot(document.getElementById('root')).render(
+try {
+  outputs = (await import("../amplify_outputs.json")).default
+} catch (err) {
+  outputs = null
+}
+
+// Only configure Amplify if outputs exist
+if (outputs) {
+  Amplify.configure(outputs)
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Index />
-  </StrictMode>,
+  </StrictMode>
 )

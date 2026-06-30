77a9e05efb55771a6a98a3d3a1b46a653a061294
nicomarklaguitan0 <nicomarklaguitan0@gmail.com>
2026-06-30 22:08:54 +0800

    update main .js

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,14 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import { Index } from "./Index.jsx"
-import "./main.css"
-import { Amplify } from "aws-amplify"
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Index } from './Index.jsx'
+import './main.css'
+import { Amplify } from "aws-amplify";
+import outputs from "../amplify_outputs.json";
 
-// SAFE: prevent Vite from trying to resolve missing file at build time
-let outputs
+Amplify.configure(outputs);
 
-if (import.meta.env.VITE_USE_AMPLIFY === "true") {
-  outputs = await import("../amplify_outputs.json")
-    .then((m) => m.default)
-    .catch(() => null)
-} else {
-  outputs = null
-}
-
-if (outputs) {
-  Amplify.configure(outputs)
-}
-
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Index />
-  </StrictMode>
+  </StrictMode>,
 )
